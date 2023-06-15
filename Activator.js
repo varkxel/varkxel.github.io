95204feb93bf7f7dbce7526b9b5545368915f8e3
varkxel <89874295+varkxel@users.noreply.github.com>
2023-06-15 04:08:49 +0100

    Burger menu on mobile

---
+++ Activator.js
@@ -0,0 +1,12 @@
+
+function Activate(id)
+{
+    const element = document.getElementById(id);
+    element.style.visibility = "visible";
+}
+
+function Deactivate(id)
+{
+    const element = document.getElementById(id);
+    element.style.visibility = "hidden";
+}
